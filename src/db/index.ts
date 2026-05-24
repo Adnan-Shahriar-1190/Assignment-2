84b8a3a7c8206b557b836ef65768855ad4221952
Adnan-Shahriar-1190 <sb.adnan2020@gmail.com>
2026-05-24 16:31:22 +0600

    vercel deploy config

--- src/db/index.ts
+++ src/db/index.ts
@@ -25,7 +25,7 @@ export const initDB = async () => {
       title varchar(150) not null,
       description text not null check (char_length(description) >= 20),
       type varchar(20) not null check (type in ('bug', 'feature_request')),
-      status varchar(20) not null default 'open' check (status in ('open', 'in_progress', 'resolved'))
+      status varchar(20) not null default 'open' check (status in ('open', 'in_progress', 'resolved')),
       reporter_id INT NOT NULL,
       created_at TIMESTAMP DEFAULT NOW(),
       updated_at TIMESTAMP DEFAULT NOW()
